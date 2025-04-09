634cc1f192ccfd51b3a26365d9494a5cee6abcc9
Pedro Dantas <pedrood1232@gmail.com>
2025-04-08 23:28:30 -0300

    Ordem personalizada das colunas

--- script3.py
+++ script3.py
@@ -11,10 +11,16 @@ print(f"Ano 2024: {len(df[df['ano'] == 2024])}")
 print(f"Semestre 1: {len(df[df['semestre'] == 1])}")
 print(f"Semestre 2: {len(df[df['semestre'] == 2])}")
 
-# Verificar colunas que sao float e pata int
+# Verificar colunas que sao float e converter para int
 for col in df.columns:
     if df[col].dtype == "float64":
         df[col] = df[col].astype("int64")
+
+# Colocar as colunas ano e semestre na treceira e quarta posicao sem saber as outras colunas
+df = df[["id_municipio","ano", "semestre"] + [col for col in df.columns if col not in ["id_municipio","ano", "semestre"]]]
+
+print(df.head())
+
 
 # Salvar tabela
 df.to_csv("fato_alfabetizacao_pre_pronto_v2.csv", index=False)
